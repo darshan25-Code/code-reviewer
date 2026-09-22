3455de7affe0df35adfb04af5a116d9e022fe2df
Darshan Raiyani <darshanraiyani63@gmail.com>
2026-09-22 21:26:30 +0530

    Initialized backend of the project

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+const connectDB = require('./config/db')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+connectDB()
+
+app.get('/api/health',(req,res)=>{
+    res.json({
+        message : "AI code-reviewer api s running"
+    })
+})
+
+app.listen(5000,()=>{
+    console.log("server is running on port 5000");
+})
